f3d38d763a3aa3518729e5a42677740f9c943a53
NabajyotiModak <naba93981@gmail.com>
2023-02-15 21:21:20 +0530

    if else

---
+++ mygolang-lco/12ifelse/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	loginCount := 23
+	var result string
+	if loginCount < 10 {
+		result = "Regular user"
+	} else if loginCount > 10 {
+		result = "Watch out"
+	} else {
+		result = "Exactly 10 login count"
+	}
+	fmt.Println(result)
+
+	if num := 3; num < 10 {
+
+		fmt.Println("Num is less than 10")
+	} else {
+
+		fmt.Println("Num is NOT less than 10")
+	}
+}
