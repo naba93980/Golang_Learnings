af03b942b4d168d323b78d61de4243bdf8a34a38
NabajyotiModak <naba93981@gmail.com>
2023-02-16 14:22:31 +0530

    defer

---
+++ mygolang-lco/17defer/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("world")
+	defer fmt.Println("one")
+	defer fmt.Println("two")
+	fmt.Println("hello")
+	myDefer();
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
+}
