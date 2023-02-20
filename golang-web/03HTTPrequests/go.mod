c4d4db1ec1494ea912dcd073fe59093344f0f1dc
NabajyotiModak <naba93981@gmail.com>
2023-02-20 23:39:15 +0530

    POST json, formdata

---
+++ golang-web/03HTTPrequests/go.mod
@@ -0,0 +1,3 @@
+module httprequests
+
+go 1.20
---
+++ golang-web/03HTTPrequests/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to web verb video - LCO")
+	// PerformGetRequest()
+	// PerformPostJsonRequest()
+	PerformPostFormRequest()
+}
+
+func PerformGetRequest() {
+	const myurl = "http://localhost:8000/"
+
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println("Status code: ", response.StatusCode)
+	fmt.Println("Content length is: ", response.ContentLength)
+
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+
+	var responseString strings.Builder
+	byteCount, _ := responseString.Write(content)
+
+	fmt.Println("ByteCount is: ", byteCount)
+	fmt.Println(responseString.String())
+
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:8000/post"
+
+	//fake json payload
+	requestBody := strings.NewReader(`
+		{
+			"coursename":"Let's go with golang",
+			"price": 0,
+			"platform":"learnCodeOnline.in"
+		}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
+}
+
+func PerformPostFormRequest() {
+	const myurl = "http://localhost:8000/postform"
+
+	//formdata
+
+	data := url.Values{}
+	data.Add("firstname", "naba")
+	data.Add("lastname", "modak")
+	data.Add("email", "naba93980@gmail.com")
+
+	response, err := http.PostForm(myurl, data)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+
+}
